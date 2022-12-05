824c71ac3bab7c89a4d3e83ce616d5b1820a733b
Rakshatnaik <60248567+Rakshatnaik@users.noreply.github.com>
2022-12-05 11:36:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let first_score = document.getElementById("score1")
+
+let second_score = document.getElementById("score2")
+
+let count1 = 0
+let count2 = 0
+
+function homeOne() {
+    count1 += 1
+    first_score.innerText =  count1  
+}
+
+
+function homeTwo() {
+    count1 += 2
+    first_score.innerText = count1
+}
+
+
+function homeThree() {
+    count1 += 3
+    first_score.innerText = count1
+}
+
+function guestOne() {
+    count2 += 1
+    second_score.innerText =  count2  
+}
+
+
+function guestTwo() {
+    count2 += 2
+    second_score.innerText = count2
+}
+
+
+function guestThree() {
+    count2 += 3
+    second_score.innerText = count2
+}
